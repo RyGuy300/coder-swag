fe4bbf300d30f22204dace74db9a80afc026a30c
Ryland Arnold <ryland@rarnold.com>
2017-10-17 19:57:43 -0400

    Added new View Controller with Collection View Cell and started building out the template

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ryland Arnold on 10/17/17.
+//  Copyright © 2017 Ryland Arnold. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
